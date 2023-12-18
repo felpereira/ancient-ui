e4d19882e95af159595f301ad0c0de6022de54d7
cb.felipe <cb.felipe@gmail.com>
2023-12-18 17:18:44 -0300

    Adicionar InputText

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
-const Input = (props: { name: string }) => {
-  return <div>{props.name}</div>;
+interface InputProps {
+  label: string;
+  name: string;
+  maxLength: number;
+  password?: boolean;
+  style?: CSSProperties;
+  styleLabel?: CSSProperties;
+}
+
+export const Input = ({
+  label,
+  name,
+  style,
+  styleLabel,
+  password = false,
+  maxLength = 20,
+  ...props
+}: InputProps) => {
+  const defaultInputStyle: CSSProperties = {
+    fontFamily: "Aileron-Regular",
+    width: password ? "80%" : "100%",
+    background: "#F8F8F8",
+    borderStyle: "none",
+    // Remover
+    outline: "none",
+  };
+
+  return (
+    <div>
+      <div style={{ ...defaultLabelStyle, ...styleLabel }}>{label}</div>
+      <div style={defaultBorderInputStyle}>
+        <input
+          name={name}
+          type={password ? "password" : "text"}
+          maxLength={maxLength}
+          style={{ ...defaultInputStyle, ...style }}
+        />
+      </div>
+    </div>
+  );
 };
 
-export default Input;
+const defaultLabelStyle: CSSProperties = {
+  paddingLeft: "5px",
+  fontFamily: "Aileron-Light",
+  fontSize: "12px",
+};
+
+const defaultBorderInputStyle: CSSProperties = {
+  padding: "5px",
+  margin: "5px",
+  borderRadius: "10px",
+  border: "1px solid #ccc",
+  fontFamily: "Aileron-Regular",
+  background: "#F8F8F8",
+};
--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -1,50 +1,37 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './Button';
+import { Input } from "../index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: "Acient-Ui/InputText",
+  component: Input,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Input>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Padrao: Story = {
+  name: "Padrao Simples",
   args: {
-    primary: true,
-    label: 'Button',
+    label: "Login",
+    name: "Input",
   },
 };
 
-export const Secondary: Story = {
+export const Senha: Story = {
+  name: "Senha",
   args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    label: "Senha",
+    name: "Input",
+    password: true,
   },
 };
