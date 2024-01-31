62510e543bc1cf1a15deb73912d387202fe81fcd
cb.felipe <cb.felipe@gmail.com>
2024-01-31 19:33:50 -0300

    .

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,25 +1,41 @@
-import React from "react";
-import styled from "./CheckBox.module.css";
+import React, { Ref, RefObject, forwardRef, useImperativeHandle } from 'react';
+
+import styled from './CheckBox.module.css';
+import { FieldError } from './Input';
+import { TextError } from './TextError';
 
 interface CheckBoxProps {
-  isChecked: boolean;
-  onClick: () => void;
-  label: string;
+    isChecked: boolean;
+    onClick: () => void;
+    label: string;
+    fieldError?: FieldError;
 }
 
-export const CheckBox = ({ isChecked, onClick, label }: CheckBoxProps) => {
-  return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <input
-        type="checkbox"
-        id={"checkbox" + label}
-        name={"checkbox" + label}
-        checked={isChecked}
-        onChange={onClick}
-      />
-      <label htmlFor={"checkbox" + label} className={styled.checkboxLabel}>
-        {label}
-      </label>
-    </div>
-  );
-};
+export const CheckBox = forwardRef<HTMLInputElement, CheckBoxProps>(
+    ({ isChecked, onClick, label, fieldError }: CheckBoxProps, ref) => {
+        return (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <input
+                    type="checkbox"
+                    id={'checkbox' + label}
+                    name={'checkbox' + label}
+                    checked={isChecked}
+                    onChange={onClick}
+                    ref={ref}
+                />
+                <label
+                    htmlFor={'checkbox' + label}
+                    className={styled.checkboxLabel}
+                >
+                    {label}
+                </label>
+                <TextError
+                    label={fieldError}
+                    size={'0.8rem'}
+                />
+            </div>
+        );
+    }
+);
+
+CheckBox.displayName = 'CheckBox';
